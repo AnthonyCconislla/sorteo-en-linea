7aff4ea73c8dcfe02e7989ba80054cdbdaff71a5
AnthonyCconislla <anthonyacconisllamedina@gmail.com>
2023-06-12 01:07:15 -0500

    página de sorteos

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -2,16 +2,25 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import icono from '../assets/icon.png'
+import Grid from '@mui/material/Grid'
 
 export default function Appbar() {
     return (
         <Box sx={{ flexGrow: 1, width: '100%' }}>
             <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, padding: '20px 20px 10PX 20PX', alignItems: 'center' }}>
-                        SORTEO EN LINEA
-                    </Typography>
-                </Toolbar>
+                <Grid container spacing={1}>
+                    <Grid item sx={{ margin: '5px 0px 0px 20px' }}>
+                        <img src={icono} alt="sorteo en linea" width={50} />
+                    </Grid>
+                    <Grid item>
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1, alignItems: 'center' }}>
+                                SORTEO EN LINEA
+                            </Typography>
+                        </Toolbar>
+                    </Grid>
+                </Grid>
             </AppBar>
         </Box>
     );

64362fcdf6e2038e55ffebc32ae8e5a4272d6f8d
AnthonyCconislla <anthonyacconisllamedina@gmail.com>
2023-06-12 01:43:39 -0500

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sorteos/"
+  base: "https://AnthonyCconislla.github.io/sorteo-en-linea"
 })
